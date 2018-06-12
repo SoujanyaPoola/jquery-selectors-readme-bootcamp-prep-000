ca15aceae1b1c2cd4afd8f336ad6185c2eb31ac1
Souji 2613 <puppypoola@gmail.com>
2018-06-12 23:00:57 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,5 +1,5 @@
 // declare your functions here...
-function paragraphSelector() {
+/*function paragraphSelector() {
   return $('p');
 }
 
@@ -17,4 +17,24 @@ function divSelector(){
 
 function firstListItem(){
   return $('#pic-list li:first')
+}*/
+
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('div.pics');
+}
+
+function firstListItem(){
+  return $('ul#pic-list li:first-child');
 }
